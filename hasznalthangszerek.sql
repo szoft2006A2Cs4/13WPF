01431f6ad314f88df20e953f9dbde46ce524ce53
Dodo-hbsz <simon.dominik@hbsz.edu.hu>
2025-11-08 19:19:00 +0100

    Add files via upload

--- hasznalthangszerek.sql
+++ hasznalthangszerek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2025 at 04:25 PM
+-- Generation Time: Nov 08, 2025 at 07:04 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -67,11 +67,10 @@ INSERT INTO `instrument` (`iid`, `uid`, `iname`, `cost`, `scname`, `description`
 (2, 4, 'Johannus Opus 150', 2000000, 'Orgona', 'Klasszikus templomi orgona 2 manuállal.', 1),
 (3, 3, 'AKAI MPK Mini MK3', 150000, 'MIDI billentyűzet', 'USB-s MIDI billentyűzet 61 billentyűvel.', 1),
 (4, 5, 'Yamaha U3', 950000, 'Zongora', 'Yamaha akusztikus zongora, kiváló hangzással.', 1),
-(5, 1, 'AW3', 850000, 'Csembaló', 'Régi stílusú barokk csembaló, díszes kivitelben.', 1),
 (6, 6, 'Roland TR-8S', 400000, 'Drum machine', 'Elektronikus dobmodul mintavételező funkcióval.', 1),
 (7, 7, 'Novation Circuit', 550000, 'Groovebox', 'Groovebox – szekvencer és szintetizátor egyben.', 1),
 (8, 8, 'Boss RC-505', 250000, 'Loop station', 'Loop station élő felvételhez és effektezéshez.', 1),
-(9, 9, 'Akai MPC One', 600000, 'Sampler', 'Professzionális Sampler DJ-k és producerek számára.', 1),
+(9, 3, 'Akai MPC One', 600000, 'Sampler', 'Professzionális Sampler DJ-k és producerek számára.', 1),
 (10, 10, 'Korg Minilogue', 1300000, 'Szintetizátor', 'Analóg szintetizátor, 4 hang polifónia.', 1),
 (11, 1, 'Moog Etherwave', 800000, 'Theremin', 'Theremin – érintés nélküli elektronikus hangszer.', 0),
 (12, 2, 'Magyar Duda 1', 90000, 'Duda', 'Klasszikus magyar duda, fából és bőrből.', 0),
@@ -81,7 +80,7 @@ INSERT INTO `instrument` (`iid`, `uid`, `iname`, `cost`, `scname`, `description`
 (16, 6, 'Bach Stradivarius', 380000, 'Harsona', 'Réz harsona, puha hangzással.', 0),
 (17, 7, 'Buffet Crampon E11', 120000, 'Klarinét', 'Klarinét, B-hangolású.', 0),
 (18, 8, 'Yamaha YHR-567', 300000, 'Kürt', 'Kürt, francia típusú.', 0),
-(19, 9, 'Loree OB-301', 400000, 'Oboa', 'Oboa fából, sötét tónusú hanggal.', 0),
+(19, 4, 'Loree OB-301', 400000, 'Oboa', 'Oboa fából, sötét tónusú hanggal.', 0),
 (20, 10, 'Pánsíp Maker', 60000, 'Pánsíp', 'Kézzel faragott pánsíp, 12 síppal.', 0),
 (21, 1, 'Yamaha YPC-32', 70000, 'Pikoló', 'Kis méretű pikoló, fuvolistáknak.', 0),
 (22, 2, 'Selmer AS42', 280000, 'Szaxofon', 'Szaxofon alt típus, arany színben.', 0),
@@ -111,16 +110,24 @@ CREATE TABLE `orderinfo` (
 --
 
 INSERT INTO `orderinfo` (`oid`, `dateOfPurchase`, `deliveryCity`, `deliveryStreet`, `deliveryPC`, `sid`, `cid`, `iid`) VALUES
-(1, '2025-09-15', 'Budapest', 'Váci utca 12', 1056, 1, 2, 1),
 (2, '2025-01-16', 'Szeged', 'Kárász utca 7', 6720, 2, 3, 2),
 (3, '2025-01-17', 'Debrecen', 'Piac utca 2', 4024, 3, 4, 3),
 (4, '2025-05-18', 'Budapest', 'Andrássy út 45', 1061, 4, 5, 4),
-(5, '2025-09-19', 'Pécs', 'Jókai tér 3', 7621, 5, 6, 5),
 (6, '2025-08-20', 'Győr', 'Széchenyi tér 4', 9021, 6, 7, 6),
 (7, '2025-05-21', 'Sopron', 'Fő tér 1', 9400, 7, 8, 7),
-(8, '2025-10-22', 'Miskolc', 'Búza tér 6', 3525, 8, 9, 8),
-(9, '2025-11-23', 'Budapest', 'Mester utca 18', 1095, 9, 10, 9),
-(10, '2025-12-24', 'Szombathely', 'Alma utca 5', 9700, 10, 1, 10);
+(175, '2025-02-13', 'Budapest', 'Rákóczi út 54', 1088, 1, 3, 11),
+(176, '2025-07-25', 'Debrecen', 'Bethlen utca 20', 4026, 2, 4, 12),
+(177, '2025-04-03', 'Szeged', 'Petőfi Sándor sugárút 3', 6722, 3, 5, 13),
+(178, '2025-11-11', 'Győr', 'Baross Gábor út 17', 9022, 4, 6, 14),
+(179, '2025-03-27', 'Pécs', 'Király utca 15', 7621, 5, 7, 15),
+(205, '2025-01-06', 'Sopron', 'Táncsics utca 8', 9400, 6, 8, 16),
+(207, '2025-05-04', 'Budapest', 'Bartók Béla út 30', 1114, 8, 10, 18),
+(209, '2025-08-08', 'Eger', 'Dobó tér 5', 3300, 10, 2, 20),
+(210, '2025-02-09', 'Nyíregyháza', 'Kossuth utca 18', 4400, 2, 5, 22),
+(211, '2025-06-17', 'Veszprém', 'Kossuth Lajos utca 7', 8200, 3, 6, 23),
+(212, '2025-07-02', 'Székesfehérvár', 'Rákóczi utca 12', 8000, 4, 7, 24),
+(213, '2025-03-14', 'Tatabánya', 'Fő utca 9', 2800, 5, 8, 25),
+(214, '2025-10-15', 'Kecskemét', 'Szabadság tér 10', 6000, 1, 4, 21);
 
 -- --------------------------------------------------------
 
@@ -211,7 +218,7 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`uid`, `uname`, `email`, `pnumber`, `password`, `review`, `postalcode`, `city`, `streetHnum`) VALUES
-(1, 'Dodo', 'dodo39766@gmail.com', 2147483647, 'enctriptedPasswordKöcsög', 5, 9700, 'Szombathely', 'Bogáti út 70'),
+(1, 'Simon Dominik', 'dodo39766@gmail.com', 2147483647, 'szeretematejet!', 5, 9700, 'Szombathely', 'Bogáti út 70'),
 (2, 'tothgabi', 'toth.gabi@example.com', 36, 'encryptedPassword1', 5, 1011, 'Budapest', 'Vörösmarty utca 12'),
 (3, 'majka', 'majka@example.com', 36, 'encryptedPassword2', 4, 1061, 'Budapest', 'Andrássy út 45'),
 (4, 'radicsgigi', 'radics.gigi@example.com', 36, 'encryptedPassword3', 5, 6720, 'Szeged', 'Kárász utca 7'),
@@ -219,8 +226,7 @@ INSERT INTO `user` (`uid`, `uname`, `email`, `pnumber`, `password`, `review`, `p
 (6, 'rubintreka', 'rubint.reka@example.com', 36, 'encryptedPassword5', 5, 1095, 'Budapest', 'Mester utca 18'),
 (7, 'konczzsuzsa', 'koncz.zsuzsa@example.com', 36, 'encryptedPassword6', 5, 7621, 'Pécs', 'Jókai tér 3'),
 (8, 'pressergabor', 'presser.gabor@example.com', 36, 'encryptedPassword7', 5, 9021, 'Győr', 'Széchenyi tér 4'),
-(9, 'torokadam', 'torok.adam@example.com', 36, 'encryptedPassword8', 4, 9400, 'Sopron', 'Fő tér 1'),
-(10, 'lljunior', 'll.junior@example.com', 36, 'encryptedPassword9', 3, 3525, 'Miskolc', 'Búza tér 6'),
+(10, 'lljunior', 'll.junior@example.com', 30, 'encryptedPassword9', 3, 3525, 'Miskolc', 'Búza tér 6'),
 (11, 'puskaspeter', 'puskas.peti@example.com', 36, 'encryptedPassword10', 5, 1012, 'Budapest', 'Futball utca 10');
 
 --
@@ -246,9 +252,9 @@ ALTER TABLE `instrument`
 --
 ALTER TABLE `orderinfo`
   ADD PRIMARY KEY (`oid`),
-  ADD UNIQUE KEY `sid` (`sid`),
-  ADD UNIQUE KEY `cid` (`cid`),
-  ADD UNIQUE KEY `iid` (`iid`);
+  ADD UNIQUE KEY `iid` (`iid`),
+  ADD KEY `orderinfo_ibfk_2` (`sid`),
+  ADD KEY `orderinfo_ibfk_3` (`cid`);
 
 --
 -- Indexes for table `subcategory`
@@ -277,7 +283,7 @@ ALTER TABLE `instrument`
 -- AUTO_INCREMENT for table `orderinfo`
 --
 ALTER TABLE `orderinfo`
-  MODIFY `oid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `oid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=215;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -303,7 +309,7 @@ ALTER TABLE `instrument`
 ALTER TABLE `orderinfo`
   ADD CONSTRAINT `orderinfo_ibfk_1` FOREIGN KEY (`iid`) REFERENCES `instrument` (`iid`),
   ADD CONSTRAINT `orderinfo_ibfk_2` FOREIGN KEY (`sid`) REFERENCES `user` (`uid`),
-  ADD CONSTRAINT `orderinfo_ibfk_3` FOREIGN KEY (`cid`) REFERENCES `user` (`uid`);
+  ADD CONSTRAINT `orderinfo_ibfk_3` FOREIGN KEY (`cid`) REFERENCES `user` (`uid`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `subcategory`
